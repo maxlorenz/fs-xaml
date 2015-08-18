1434306f3e25a818e0e378b402e433befa8fdf4c
Max <max-lorenz@gmx.net>
2015-08-18 14:14:21 +0200

    Implemented a simple Window in XAML

---
+++ fs-xaml/MainWindow.fs
@@ -0,0 +1,19 @@
+﻿module MainWindow
+
+open System.Windows.Controls
+
+open UI
+
+let configure window =
+    
+    let btnClick    :Button     = window?btnClick
+    let txtInput    :TextBox    = window?txtInput
+
+    btnClick.Click.Add(fun _ -> txtInput.Text <- sprintf "Clicked at %A" System.DateTime.Now)
+
+let run() =
+    
+    "MainWindow.fs.xaml"
+    |> UI.load
+    |> UI.configure configure
+    |> UI.run
---
+++ fs-xaml/UI.fs
@@ -0,0 +1,27 @@
+﻿module UI
+
+open System
+open System.Reflection
+open System.Windows
+open System.Windows.Controls
+
+let (?) (c:obj) (s:string) =
+    match c with
+    | :? ResourceDictionary as r -> r.[s] :?> 'T
+    | :? Control as c -> c.FindName(s) :?> 'T
+    | _ -> failwith "dynamic lookup failed"
+
+let load name =
+    let applicationName = Assembly.GetExecutingAssembly().FullName
+    let resource = Uri(sprintf "/%s;component/%s" applicationName name, System.UriKind.Relative)
+
+    Application.LoadComponent(resource) :?> Window
+
+let configure (configure: Window -> unit) window =
+    configure window; window
+
+let run window =
+    do (Application()).Run(window) |> ignore
+
+let show (window: Window) =
+    window.Show()
